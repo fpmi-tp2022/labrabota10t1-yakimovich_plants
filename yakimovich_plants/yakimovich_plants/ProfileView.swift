7262adb3da828c8025da26d5644d204c60ce431c
Depravo <depravo@Depravos-iMac.local>
2022-06-21 10:50:02 -0700

    add sign in page

---
+++ yakimovich_plants/yakimovich_plants/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  yakimovich_plants
+//
+//  Created by Depravo on 21.06.2022.
+//
+
+import Foundation
+import UIKit
+
+class ProfileView : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       // Do any additional setup after loading the view.
+    }
+    
+}
